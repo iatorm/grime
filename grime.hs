31a38bc5ca31c8bcaffb2513e7cfd4d25f02b27b
Ilkka Törmä <iatorm@utu.fi>
2015-04-02 20:53:56 +0300

    Added size constraints.

--- grime.hs
+++ grime.hs
@@ -8,7 +8,7 @@ import Control.Monad (forM, forM_, liftM2, (>=>), filterM, when)
 import Control.Monad.Trans.Reader (ReaderT, asks, runReaderT)
 import Control.Monad.State.Lazy (State, gets, modify, evalState)
 import Text.Parsec (Parsec, ParseError, parse, try, (<?>), (<|>), between, many, manyTill, many1, choice, optionMaybe)
-import Text.Parsec.Char (char, oneOf, noneOf, anyChar, string, upper, lower)
+import Text.Parsec.Char (char, oneOf, noneOf, anyChar, string, upper, lower, digit)
 import Text.Parsec.Expr
 import System.Environment (getArgs)
 
@@ -18,20 +18,22 @@ type Label = Maybe Char
 type Coord = (Int, Int) -- x, y
 type Size = (Int, Int) -- w, h
 type Rect = (Int, Int, Int, Int) -- x, y, w, h
+type Range = (Int, Maybe Int)
 
 -- An expression that may or may not match a rectangle of characters
-data Expr = Empty               -- Mathces a 0xn or nx0 rectangle
-          | Border              -- Matches the rectangle border symbol
-          | AnyChar             -- Matches any single character (not border)
-          | SomeChar (Set Char) -- Matches any of the given characters
-          | Var Label           -- Matches the given variable
-          | Expr :> Expr        -- Horizontal concatenation
-          | HPlus Expr          -- Horizontal repetition
-          | Expr :^ Expr        -- Vertical concatenation
-          | VPlus Expr          -- Vertical repetition
-          | Expr :| Expr        -- Disjunction
-          | Expr :& Expr        -- Conjunction
-          | Not Expr            -- Negation
+data Expr = Empty                  -- Mathces a 0xn or nx0 rectangle
+          | Border                 -- Matches the rectangle border symbol
+          | AnyChar                -- Matches any single character (not border)
+          | SomeChar (Set Char)    -- Matches any of the given characters
+          | Var Label              -- Matches the given variable
+          | Expr :> Expr           -- Horizontal concatenation
+          | HPlus Expr             -- Horizontal repetition
+          | Expr :^ Expr           -- Vertical concatenation
+          | VPlus Expr             -- Vertical repetition
+          | Expr :| Expr           -- Disjunction
+          | Expr :& Expr           -- Conjunction
+          | Not Expr               -- Negation
+          | Sized Range Range Expr -- Size range
 
 instance Show Expr where
   show Empty = "_"
@@ -47,6 +49,10 @@ instance Show Expr where
   show (e1 :| e2) = "(" ++ show e1 ++ "|" ++ show e2 ++ ")"
   show (e1 :& e2) = "(" ++ show e1 ++ "&" ++ show e2 ++ ")"
   show (Not e) = "(" ++ show e ++ ")!"
+  show (Sized (x1,x2) (y1,y2) e) =
+    show e ++ "{" ++ show x1 ++ "-" ++ sx2 ++ "," ++ show y1 ++ "-" ++ sy2 ++ "}"
+    where sx2 = case x2 of Nothing -> ""; Just x -> show x
+          sy2 = case y2 of Nothing -> ""; Just y -> show y
 
 mkSomeChar :: String -> Expr
 mkSomeChar = SomeChar . fromAscList . sort
@@ -54,7 +60,8 @@ mkSomeChar = SomeChar . fromAscList . sort
 -- Expression parser
 pExpr :: Parsec String () Expr
 pExpr = buildExpressionParser opTable term <?> "expression"
-  where term = try (parens pExpr) <|> try escaped <|> try charClass <|> reserved <?> "term"
+  where term = choice (map try
+               [parens pExpr, escaped, charClass, reserved]) <?> "term"
         escaped = do
           char '\\'
           c <- anyChar
@@ -87,13 +94,29 @@ pExpr = buildExpressionParser opTable term <?> "expression"
             (False, Nothing) -> mkSomeChar $ concat pos
             (True, Just negs) -> AnyChar :& (Not . mkSomeChar $ concat negs)
             (False, Just negs) -> mkSomeChar $ concat pos \\ concat negs
+        numRange = do
+          lower <- many digit
+          maybeDash <- optionMaybe $ char '-'
+          upper <- many digit
+          let lowerNum = if null lower then 0 else read lower
+              upperNum = if null upper then Nothing else Just $ read upper
+          return $ case maybeDash of
+            Nothing -> (lowerNum, if null lower then Nothing else Just lowerNum)
+            Just _ -> (lowerNum, upperNum)
+        sizeRange = between (char '{') (char '}') $ do
+          xRange <- numRange
+          maybeYRange <- optionMaybe $ char ',' >> numRange
+          return $ case maybeYRange of
+            Nothing -> Sized xRange xRange
+            Just yRange -> Sized xRange yRange
         opTable = [[Postfix postfix],
                    [Infix (return (:>)) AssocLeft],
                    [Infix (char '/' >> return (:^)) AssocLeft],
                    [Infix (char '&' >> return (:&)) AssocLeft],
                    [Infix (char '|' >> return (:|)) AssocLeft]]
         postfix = fmap (foldr1 (.) . reverse) . many1 . choice . map try $
-          [char '?' >> return (Empty :|),
+          [sizeRange,
+           char '?' >> return (Empty :|),
            char '+' >> return HPlus,
            char '*' >> return (\e -> Empty :| HPlus e),
            string "/+" >> return VPlus,
@@ -154,6 +177,9 @@ f |? g = do
 anyM :: (Monad m) => [a] -> (a -> m Bool) -> m Bool
 anyM xs f = foldr (|?) (return False) $ map f xs
 
+allM :: (Monad m) => [a] -> (a -> m Bool) -> m Bool
+allM xs f = foldr (&?) (return True) $ map f xs
+
 -- Does the pattern match? Update all sub-rectangles as needed
 matches :: Expr -> Rect -> Matcher Bool
 matches Empty (_, _, w, h) = return $ w == 0 || h == 0
@@ -200,7 +226,16 @@ matches (VPlus expr) r@(x, y, w, h) = whole |? parts
 matches (exp1 :| exp2) rect = matches exp1 rect |? matches exp2 rect
 matches (exp1 :& exp2) rect = matches exp1 rect &? matches exp2 rect
 matches (Not expr) rect = fmap not $ matches expr rect
-
+matches (Sized (x1, x2) (y1, y2) exp) r@(x, y, w, h) = do
+  let xOk = x1 <= w && case x2 of Nothing -> True; Just x3 -> w <= x3
+      yOk = y1 <= h && case y2 of Nothing -> True; Just y3 -> h <= y3
+  return (xOk && yOk) &? case exp of
+    Border -> allMatch
+    AnyChar -> allMatch
+    SomeChar _ -> allMatch
+    _ -> matches exp r
+  where allMatch = allM [(x+i, y+j, 1, 1) | i <- [0..w-1], j <- [0..h-1]] $
+                   matches exp
 
 
 -- Collect matches of Nothing for the given rectangles.
